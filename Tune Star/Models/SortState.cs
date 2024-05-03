dc8945a6ab339e9b61e5ff2d56f782c8de916c99
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-03 12:29:11 +0100

    View Models

---
+++ Tune Star/Models/SortState.cs
@@ -0,0 +1,12 @@
+﻿namespace Tune_Star.Models
+{
+    public enum SortState
+    {
+        NameAsc,
+        NameDesc,
+        ArtistAsc,
+        ArtistDesc,
+        GenreAsc,
+        GenreDesc 
+    }
+}
